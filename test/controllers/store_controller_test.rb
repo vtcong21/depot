af8956287fd7d25c81ed7a02fb2a1e1bc168825c
vtcong21 <vtcong21@vp.fitus.edu.vn>
2024-10-03 16:27:22 +0700

    done index

---
+++ test/controllers/store_controller_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class StoreControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get store_index_url
+    assert_response :success
+    assert_select "nav a", minimum: 4
+    assert_select "main ul li", 3
+  end
+end
