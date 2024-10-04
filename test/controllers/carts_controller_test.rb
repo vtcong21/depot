20ab7dc792d6e1966b89812c1ca01b7f3e7022f1
vtcong21 <vtcong21@vp.fitus.edu.vn>
2024-10-04 08:41:34 +0700

    add csr

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -37,14 +37,6 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
     patch cart_url(@cart), params: { cart: {} }
     assert_redirected_to cart_url(@cart)
   end
-
-  test "should destroy cart" do
-    assert_difference("Cart.count", -1) do
-      delete cart_url(@cart)
-    end
-
-    assert_redirected_to carts_url
-  end
   test "should destroy cart" do
     post line_items_url, params: { product_id: products(:ruby).id }
     @cart = Cart.find(session[:cart_id])
